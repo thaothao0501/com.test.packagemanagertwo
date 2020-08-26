5828a7ddfb648a58bc54cbdefdbc27c56b4a268d
thaothao0501 <70183199+thaothao0501@users.noreply.github.com>
2020-08-26 08:52:59 +0700

    Add files via upload

---
+++ Runtime/GameUtilitie.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Com.Test {
+    public class GameUtilitie : MonoBehaviour {
+        public static string Version = "1.0.0";
+        public static string SomeThingElse = "Some Thing Else";
+        // Start is called before the first frame update
+        void Start() {
+
+        }
+
+        // Update is called once per frame
+        void Update() {
+
+        }
+    }
+}
